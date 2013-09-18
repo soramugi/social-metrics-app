befce07a38af69ff7882a6b36504621999ab8214
soramugi <soramugi.chika@gmail.com>
2013-09-19 02:10:14 +0900

    form使っていい感じに表示するように

--- app.rb
+++ app.rb
@@ -1,7 +1,18 @@
 require 'sinatra'
 
+class URI::HTTP
+  def not_scheme
+    self.host + self.request_uri
+  end
+end
+
 get'/' do
-  uri = 'https://www.google.co.jp/'
-  @uris = [ uri  ]
+  @uris = [ URI.parse(request.url) ]
+  haml :index
+end
+
+post'/' do
+  @uris = []
+  @uris.push(URI.parse(params[:url])) if params[:url].include?('http')
   haml :index
 end
