a02499d17d2c28255025d73d0bee62e64bf86a21
cube <cube@sonicmoov.com>
2013-09-19 20:39:17 +0900

    いい感じに見せる

--- app.rb
+++ app.rb
@@ -9,7 +9,8 @@ class URI::HTTP
 end
 
 get'/' do
-  @uris = [ URI.parse(request.url) ]
+  @uri  = URI.parse(request.url)
+  @uris = [ @uri ]
   haml :index
 end
 
@@ -17,13 +18,15 @@ post'/' do
   @uris = []
   if params[:url].include?('http')
     begin
+      @uri = URI.parse(params[:url])
       feeds = FeedSearcher.search(params[:url])
       rss = RSS::Parser.parse(feeds.first)
       rss.items.each do |item|
         @uris.push(URI.parse(item.link.href))
       end
     rescue
-      @uris.push(URI.parse(params[:url]))
+      @uri = URI.parse(params[:url])
+      @uris.push(@uri)
     end
   end
   haml :index
