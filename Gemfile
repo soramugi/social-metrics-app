637f721e0ac51b6a88d757f46a509e7bf24fa05a
cube <cube@sonicmoov.com>
2013-09-19 11:45:58 +0900

    test

--- Gemfile
+++ Gemfile
@@ -6,3 +6,4 @@ gem "shotgun"
 gem "haml"
 gem "nokogiri", '1.5.0'
 gem "feed_searcher"
+gem "rack-test"
---
+++ test/app_test.rb
@@ -0,0 +1,23 @@
+require File.expand_path(File.dirname(__FILE__)) + '/../app'
+require 'test/unit'
+require 'rack/test'
+
+ENV['RACK_ENV'] = 'test'
+
+class AppTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_it_says
+    get '/'
+    assert last_response.ok?
+  end
+
+  def test_it_says_post
+    post '/', url: ''
+    assert last_response.ok?
+  end
+end
